3646497edaa07a02de3ba765ac9a3dcf92044014
Leonardo Treviso <leonardosistemas@outlook.com>
2024-10-04 11:31:48 -0300

    ajustando conexão

--- src/models/db.js
+++ src/models/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Configurando a conexão com o banco de dados MySQL
 const pool = mysql.createPool({
-  host: 'localhost', // Endereço do servidor MySQL
-  user: 'root', // Seu usuário do MySQL
-  password: '', // Sua senha do MySQL
-  database: 'to-dolist' // Nome do banco de dados que você vai usar
+  host: 'db4free.net', // Endereço do servidor MySQL
+  user: 'tempovalioso', // Seu usuário do MySQL
+  password: 'tempovalioso', // Sua senha do MySQL
+  database: 'tempovalioso' // Nome do banco de dados que você vai usar
 });
 
 module.exports = pool.promise();
